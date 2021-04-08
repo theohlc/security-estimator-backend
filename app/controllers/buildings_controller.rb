8f9d40dec7f42831df469cbef36d8406c4059ac7
theo <teddyhoover@gmail.com>
2021-04-08 14:06:03 -0500

    added index, show

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,11 @@
 class BuildingsController < ApplicationController
+    def index
+        buildings = Building.all
+        render json: BuildingsSerializer.new(buildings).to_serialized_json
+    end
+
+    def show
+        building = Building.find(params[:id])
+        render json: BuildingsSerializer.new(building).to_serialized_json
+    end
 end

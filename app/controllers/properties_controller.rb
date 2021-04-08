a34bdec57c1077fb05b00a3794582775d1f5d9c3
theo <teddyhoover@gmail.com>
2021-04-08 13:59:24 -0500

    added index, show

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,11 @@
 class PropertiesController < ApplicationController
-    
+    def index
+        properties = Property.all
+        render json: PropertiesSerializer.new(properties).to_serialized_json
+    end
+
+    def show
+        property = Property.find(params[:id])
+        render json: PropertiesSerializer.new(property).to_serialized_json
+    end
 end
---
+++ app/services/properties_serializer.rb
@@ -0,0 +1,17 @@
+class PropertiesSerializer
+    def initialize(property)
+        @property = property
+    end
+
+    def to_serialized_json
+        options = {
+          include: {
+            buildings: {
+              methods: :cost 
+            }
+          },
+          except: [:updated_at],
+        }
+        @property.to_json(options)
+      end 
+end

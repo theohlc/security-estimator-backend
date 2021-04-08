8bdce57cb052437ef1c40f55cc74bce5a839f2ba
theo <teddyhoover@gmail.com>
2021-04-08 13:17:20 -0500

    added cost method

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,12 @@
 class Building < ApplicationRecord
   belongs_to :property
+
+  def cost
+    cost = 1000
+    cost += 160 * num_ground_windows
+    cost += 50 * num_high_windows
+    cost += 100 * num_doors
+    cost += 200 * num_vehicle_doors
+  end
+
 end

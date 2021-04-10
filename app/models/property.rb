df09eaff0fd3510f404bae6113980897bc3e17b7
theo <teddyhoover@gmail.com>
2021-04-10 17:16:56 -0500

    added property cost

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,14 @@
 class Property < ApplicationRecord
     has_many :buildings
+    validates :name,           presence: true
+    validates :fence_length,   presence: true
+
+    def cost
+        cost = fence_length*5
+        self.buildings.each do |building|
+            cost += building.cost
+        end
+        cost
+    end
+
 end

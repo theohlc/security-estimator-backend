d56fffeb8cd057f16fd7a47189aeda82083d756d
theo <teddyhoover@gmail.com>
2021-04-11 15:41:46 -0500

    added fence_cost

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,11 +4,15 @@ class Property < ApplicationRecord
     validates :fence_length,   presence: true
 
     def cost
-        cost = fence_length*5
+        cost = fence_cost
         self.buildings.each do |building|
             cost += building.cost
         end
         cost
+    end
+
+    def fence_cost
+        fence_length*5
     end
 
 end
